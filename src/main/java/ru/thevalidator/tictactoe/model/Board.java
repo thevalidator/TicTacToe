5f2f8e0dc678bd6da602c77b5f00705cb6c28bfa
illusion <the.validator@yandex.ru>
2023-09-22 13:34:01 +0300

    inject jPanel, clear board improoved

--- src/main/java/ru/thevalidator/tictactoe/model/Board.java
+++ src/main/java/ru/thevalidator/tictactoe/model/Board.java
@@ -3,6 +3,8 @@
  */
 package ru.thevalidator.tictactoe.model;
 
+import javax.swing.JPanel;
+
 /**
  * @author thevalidator
  */
@@ -10,9 +12,14 @@ public class Board {
 
     public static final int BOARD_SIZE = 3;
     private final int[][] board;
+    private JPanel gamePanel;
 
     public Board() {
         board = new int[BOARD_SIZE][BOARD_SIZE];
+    }
+
+    public void setPanel(JPanel panel) {
+        this.gamePanel = panel;
     }
 
     public int getboxValue(int x, int y) {
@@ -29,6 +36,19 @@ public class Board {
     
     public int getHorisontalSze() {
         return board[0].length;
+    }
+    
+    public void clearBoard() {
+        clearValues();
+        gamePanel.repaint();
+    }
+
+    private void clearValues() {
+        for (int[] row: board) {
+            for (int i = 0; i < row.length; i++) {
+                row[i] = 0;
+            }
+        }
     }
 
 }
