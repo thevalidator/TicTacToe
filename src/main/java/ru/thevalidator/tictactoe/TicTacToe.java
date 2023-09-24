2da0e904dde248ac8a0a9a2e71641cbb5d60b8ee
illusion <the.validator@yandex.ru>
2023-09-24 20:45:55 +0300

    Create TicTacToe.java

---
+++ src/main/java/ru/thevalidator/tictactoe/TicTacToe.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.tictactoe;
+
+import ru.thevalidator.tictactoe.gui.MainWindow;
+
+/**
+ *
+ * @author thevalidator
+ */
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        MainWindow.start(null);
+    }
+    
+}
