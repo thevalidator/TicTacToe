9b4d8cb179b1bb372e033c0c5401b7f0a9c6eaab
illusion <the.validator@yandex.ru>
2023-09-24 20:33:44 +0300

    fix comments

--- src/main/java/ru/thevalidator/tictactoe/gui/Settings.java
+++ src/main/java/ru/thevalidator/tictactoe/gui/Settings.java
@@ -15,12 +15,11 @@ public class Settings {
     public static final int MAIN_WINDOW_HEIGHT = MAIN_WINDOW_WIDTH + 100;
     
     
-    //WIN COLOR #ED1F1F
+    //WIN COLOR #00F302 //#ED1F1F
     //CIRCLE COLOR #AFAFAF
     //CROSS COLOR #58618A
     
     //<a target="_blank" href="https://icons8.com/icon/eTofvMVD4UUm/circle">Circle</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
     //<a target="_blank" href="https://icons8.com/icon/RvdYzOup1Xqx/multiply">Multiply</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
-    //<a target="_blank" href="https://icons8.com/icon/vvHsCpLe200a/horizontal-line">Horizontal Line</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
 
 }
